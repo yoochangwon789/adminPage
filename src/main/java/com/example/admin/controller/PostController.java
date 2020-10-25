b1311efa9f86dcb1421f1fecde7005dbbac9816b
changwon <ckddnjs554@naver.com>
2020-10-26 03:53:04 +0900

    Lombok 라이브러리 추가

--- src/main/java/com/example/admin/controller/PostController.java
+++ src/main/java/com/example/admin/controller/PostController.java
@@ -17,5 +17,5 @@ public class PostController {
 
         return searchParam;
     }
-    
+
 }
--- src/main/java/com/example/admin/model/SearchParam.java
+++ src/main/java/com/example/admin/model/SearchParam.java
@@ -1,5 +1,10 @@
 package com.example.admin.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SearchParam {
 
     private String account;
@@ -9,28 +14,4 @@ public class SearchParam {
     private int page;
 
     // { "account : "", "email" : "", "page" : 0 }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
