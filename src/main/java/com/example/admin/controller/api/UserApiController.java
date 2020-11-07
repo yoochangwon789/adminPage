41ea86db7fc611f3ff9c412fac2f71b5d283376e
changwon <ckddnjs554@naver.com>
2020-11-08 00:49:56 +0900

    User_Service_Read

--- src/main/java/com/example/admin/controller/api/UserApiController.java
+++ src/main/java/com/example/admin/controller/api/UserApiController.java
@@ -5,9 +5,11 @@ import com.example.admin.model.network.Header;
 import com.example.admin.model.network.request.UserApiRequest;
 import com.example.admin.model.network.response.UserApiResponse;
 import com.example.admin.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j  // java 에서 쓰는 loge System 데이터가 어떻게 움직이는지 콘솔창에 흔적을 보기 위해 쓰는 기능 sout 대신 사용 test가 아닌 실제 프로젝트에서는 이것을 많이 사용함
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
@@ -17,19 +19,21 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
 
     @Override
     @PostMapping("")    // /api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> userApiRequest) {
-        return userApiLogicService.create(userApiRequest);
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}",request);
+        return userApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}") // api/user/{id}
     public Header<UserApiResponse> read(@PathVariable Long id) {
-        return null;
+        log.info("read id : {}", id);
+        return userApiLogicService.read(id);
     }
 
     @Override
     @PutMapping("") // api/user
-    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
         return null;
     }
 
