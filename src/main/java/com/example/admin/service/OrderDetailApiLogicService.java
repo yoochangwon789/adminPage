3847391386a736b09d45a8e28558c10574ea94b8
changwon <ckddnjs554@naver.com>
2020-11-10 23:56:33 +0900

    OrderDetail_Create

--- src/main/java/com/example/admin/service/OrderDetailApiLogicService.java
+++ src/main/java/com/example/admin/service/OrderDetailApiLogicService.java
@@ -1,14 +1,18 @@
 package com.example.admin.service;
 
 import com.example.admin.ifs.CrudInterface;
+import com.example.admin.model.entity.OrderDetail;
 import com.example.admin.model.network.Header;
 import com.example.admin.model.network.request.OrderDetailApiRequest;
+import com.example.admin.model.network.request.OrderGroupApiRequest;
 import com.example.admin.model.network.response.OrderDetailApiResponse;
 import com.example.admin.repository.ItemRepository;
 import com.example.admin.repository.OrderDetailRepository;
 import com.example.admin.repository.OrderGroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class OrderDetailApiLogicService implements CrudInterface<OrderDetailApiRequest, OrderDetailApiResponse> {
@@ -24,6 +28,20 @@ public class OrderDetailApiLogicService implements CrudInterface<OrderDetailApiR
 
     @Override
     public Header<OrderDetailApiResponse> create(Header<OrderDetailApiRequest> request) {
+
+        OrderDetailApiRequest body = request.getData();
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .status(body.getStatus())
+                .arrivalDate(LocalDateTime.now().plusDays(2))
+                .quantity(body.getQuantity())
+                .totalPrice(body.getTotalPrice())
+                .item(itemRepository.getOne(body.getItemId()))
+                .orderGroup(orderGroupRepository.getOne(body.getOrderGroupId()))
+                .build();
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+
         return null;
     }
 
@@ -39,6 +57,10 @@ public class OrderDetailApiLogicService implements CrudInterface<OrderDetailApiR
 
     @Override
     public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<OrderDetailApiResponse> response(OrderDetail orderDetail) {
         return null;
     }
 }
