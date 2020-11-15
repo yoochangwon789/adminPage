09e6a3ca3b6c99d07a43a12d3e4cb40a4a4163a9
changwon <ckddnjs554@naver.com>
2020-11-16 00:21:27 +0900

    Code_Refactoring_ItemTable

--- src/main/java/com/example/admin/controller/api/ItemApiController.java
+++ src/main/java/com/example/admin/controller/api/ItemApiController.java
@@ -1,43 +1,28 @@
 package com.example.admin.controller.api;
 
-import com.example.admin.ifs.CrudInterface;
-import com.example.admin.model.network.Header;
+import com.example.admin.controller.CrudController;
 import com.example.admin.model.network.request.ItemApiRequest;
 import com.example.admin.model.network.response.ItemApiResponse;
 import com.example.admin.service.ItemApiLogicService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return itemApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return itemApiLogicService.delete(id);
+    // CrudController 에 있는 baseService 의 객체를 채워 줘야 한다 왜냐하면 baseService 에 내가 원하는 테이블의 객체를 넣어 줘야 지
+    // CrudController 가 작동해서 내가 원하는 mapping 의 값을 얻을 수 있다.
+    @PostConstruct  // 초기 생성자 메서드 같은 의미
+    public void init() {
+        this.baseService = itemApiLogicService;
     }
 }
