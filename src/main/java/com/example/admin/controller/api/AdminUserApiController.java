3670f3de33bad8a3e56c86679ad3392bc69a90f3
changwon <ckddnjs554@naver.com>
2020-11-19 23:32:49 +0900

    Controller_abstract_finish

--- src/main/java/com/example/admin/controller/api/AdminUserApiController.java
+++ src/main/java/com/example/admin/controller/api/AdminUserApiController.java
@@ -1,42 +1,24 @@
 package com.example.admin.controller.api;
 
-import com.example.admin.ifs.CrudInterface;
-import com.example.admin.model.network.Header;
+import com.example.admin.controller.CrudController;
 import com.example.admin.model.network.request.AdminUserApiRequest;
 import com.example.admin.model.network.response.AdminUserApiResponse;
 import com.example.admin.service.AdminUserApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/adminUser")
-public class AdminUserApiController implements CrudInterface<AdminUserApiRequest, AdminUserApiResponse> {
+public class AdminUserApiController extends CrudController<AdminUserApiRequest, AdminUserApiResponse> {
 
     @Autowired
     private AdminUserApiLogicService adminUserApiLogicService;
 
-
-    @Override
-    @PostMapping("")
-    public Header<AdminUserApiResponse> create(@RequestBody Header<AdminUserApiRequest> request) {
-        return adminUserApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header<AdminUserApiResponse> read(@PathVariable Long id) {
-        return adminUserApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<AdminUserApiResponse> update(@RequestBody Header<AdminUserApiRequest> request) {
-        return adminUserApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return adminUserApiLogicService.delete(id);
+    @PostConstruct
+    public void init() {
+        this.baseService = adminUserApiLogicService;
     }
 }
