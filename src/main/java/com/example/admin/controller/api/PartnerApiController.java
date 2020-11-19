c0f52834c15ac9d4587d9a4ae2da14a36b1086c7
changwon <ckddnjs554@naver.com>
2020-11-19 23:29:08 +0900

    Controller_abstract_PartnerTable

--- src/main/java/com/example/admin/controller/api/PartnerApiController.java
+++ src/main/java/com/example/admin/controller/api/PartnerApiController.java
@@ -1,41 +1,24 @@
 package com.example.admin.controller.api;
 
-import com.example.admin.ifs.CrudInterface;
-import com.example.admin.model.network.Header;
+import com.example.admin.controller.CrudController;
 import com.example.admin.model.network.request.PartnerApiRequest;
 import com.example.admin.model.network.response.PartnerApiResponse;
 import com.example.admin.service.PartnerApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/partner")
-public class PartnerApiController implements CrudInterface<PartnerApiRequest, PartnerApiResponse> {
+public class PartnerApiController extends CrudController<PartnerApiRequest, PartnerApiResponse> {
 
     @Autowired
     private PartnerApiLogicService partnerApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<PartnerApiResponse> create(@RequestBody Header<PartnerApiRequest> request) {
-        return partnerApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header<PartnerApiResponse> read(@PathVariable Long id) {
-        return partnerApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<PartnerApiResponse> update(@RequestBody Header<PartnerApiRequest> request) {
-        return partnerApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return partnerApiLogicService.delete(id);
+    @PostConstruct
+    public void init() {
+        this.baseService = partnerApiLogicService;
     }
 }
